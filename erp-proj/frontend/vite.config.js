1a5e338b57654804fde259b029c52a6da9c60531
ericjstocker <60724489+ericjstocker@users.noreply.github.com>
2026-03-27 08:44:59 -0500

    Add files via upload

--- erp-proj/frontend/vite.config.js
+++ erp-proj/frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['erp.ancibel.net'],
+    allowedHosts: ['erp.ancibel.net', 'localhost'],
     hmr: false
   }
 })
