8895336c6e95024c590ab21ab1fd96e6489b0c7e
ericjstocker <60724489+ericjstocker@users.noreply.github.com>
2026-03-27 11:54:15 -0500

    Add files via upload

--- erp-proj/backend/alembic/versions/0001_initial.py
+++ erp-proj/backend/alembic/versions/0001_initial.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '0001_initial'
+revision = '0001'
 down_revision = None
 branch_labels = None
 depends_on = None
